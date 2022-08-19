e13f219a6551c3227084eb114e058c8a7f92f56d
Agustin Espana <agustinespania@gmail.com>
2022-08-19 15:50:26 +0200

    added method to extract html anchors

--- src/main/java/utils/HtmlExtractor.java
+++ src/main/java/utils/HtmlExtractor.java
@@ -1,7 +1,9 @@
 package utils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,6 +20,32 @@ public class HtmlExtractor {
             rawTitle.append(matcher.group());
 
         return rawTitle.substring(7, rawTitle.length() - 8);
+    }
 
+    public static Set<String> getAnchorList(String html) {
+        Pattern regex = Pattern.compile("href=\"/(.*?)\"");
+        Set<String> pathList = new HashSet<>();
+        List<String> links = extractLinks(regex, html);
+
+        for (String s : links) {
+            int startIndex = 7; // removes the html tag
+            String anchor = s.substring(startIndex, s.length() -1);
+            if (!anchor.contains("."))
+                pathList.add(anchor);
+        }
+
+        return pathList;
+    }
+
+    private static List<String> extractLinks(Pattern regex, String html) {
+        List<String> matchList = new ArrayList<>();
+
+        Matcher matcher = regex.matcher(html);
+
+        while (matcher.find()) {
+            matchList.add(matcher.group());
+        }
+
+        return matchList;
     }
 }
