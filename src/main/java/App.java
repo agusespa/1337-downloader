3fae01f1954e4392b4c671a995527a3b6580f8e9
Agustin Espana <agustinespania@gmail.com>
2022-08-19 15:46:44 +0200

    added recursive traversal

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,5 +15,41 @@ public class App {
 
         String anchor = ""; // home page
         recursiveTraverse(baseUrl, anchor, visitedAnchors);
+    }
+
+
+    static void recursiveTraverse(String baseUrl, String anchor, Set<String> visitedAnchors) {
+
+        visitedAnchors.add(anchor);
+        Set<String> anchors = new HashSet<>();
+
+        try {
+            URL url = new URL(baseUrl + anchor);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            StringBuilder parsedHtml = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                parsedHtml.append(line);
+            }
+            reader.close();
+
+            String title = HtmlExtractor.extractTitle(parsedHtml.toString());
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(title + ".html"));
+            writer.write(parsedHtml.toString());
+            writer.close();
+
+            anchors = HtmlExtractor.getAnchorList(parsedHtml.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String a : anchors) {
+            if (!visitedAnchors.contains(a)) {
+                recursiveTraverse(baseUrl, a, visitedAnchors);
+            }
+        }
     }
 }
