3edd265b438cbf92e056ccea8afd67b2a0c1bd0e
Agustin Espana <agustinespania@gmail.com>
2022-08-20 09:46:26 +0200

    tests for new path parsing method

--- src/test/java/utils/FileDownloaderTest.java
+++ src/test/java/utils/FileDownloaderTest.java
@@ -10,12 +10,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FileDownloaderTest {
 
-    FileDownloader fileDownloader = new FileDownloader("https://tretton37.com/", "assets/i/join.jpg", "assets/i");
+    FileDownloader fileDownloader = new FileDownloader("https://tretton37.com/", "assets/i/join.jpg");
 
     @AfterAll
     public void cleanUp() throws IOException {
@@ -26,13 +27,29 @@ public class FileDownloaderTest {
 
     @Test
     @Order(1)
+    public void shouldReturnValidDirectoryPath() {
+        String returned = fileDownloader.getDirectoryString("assets/i/join.jpg");
+        String expected = "assets/i";
+        assertEquals(expected, returned);
+    }
+
+    @Test
+    @Order(2)
+    public void shouldReturnEmptyDirectoryPathForFileAtRoot() {
+        String returned = fileDownloader.getDirectoryString("join.jpg");
+        String expected = "";
+        assertEquals(expected, returned);
+    }
+
+    @Test
+    @Order(3)
     public void shouldMakeDir() {
         fileDownloader.makeDirs();
         assertTrue(Files.isDirectory(Paths.get("assets/i")));
     }
 
     @Test
-    @Order(2)
+    @Order(4)
     public void shouldDownloadFile() {
         fileDownloader.makeDirs();
         fileDownloader.downloadFile();
